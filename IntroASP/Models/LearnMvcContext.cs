e863ab101cf8865a30e1a272746783640376092b
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-01 18:25:22 -0600

    initial project :tada:

---
+++ IntroASP/Models/LearnMvcContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Models;
+
+public partial class LearnMvcContext : DbContext
+{
+    public LearnMvcContext()
+    {
+    }
+
+    public LearnMvcContext(DbContextOptions<LearnMvcContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beer> Beers { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost; Database=LearnMVC; Trusted_Connection=True; Encrypt=false;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beer>(entity =>
+        {
+            entity.HasKey(e => e.BeerId).HasName("PK__Beer__293C94BFE3D59CFF");
+
+            entity.ToTable("Beer");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Beers)
+                .HasForeignKey(d => d.BrandId)
+                .HasConstraintName("FK__Beer__BrandId__4BAC3F29");
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.BrandId).HasName("PK__Brand__DAD4F05E7AB4FB93");
+
+            entity.ToTable("Brand");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
